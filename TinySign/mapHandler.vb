2410c2d520b34a7adb57293601b447290e57d61a
Sean <shorton@worcester.edu>
2014-05-16 22:48:02 -0400

    This finds map name from file

--- TinySign/mapHandler.vb
+++ TinySign/mapHandler.vb
@@ -3,6 +3,19 @@
 Public Class mapHandler
 
     Public Sub arrayToString(array As Byte())
+        Dim charArray(20) As Char
+
+        'put the byte array into a char array
+        Dim index As Integer = 0
+        Do Until index >= 19
+            If array(index) = 0 Then Exit Do
+            charArray(index) = Convert.ToChar(array(index))
+            index += 1
+        Loop
+
+        'make char array into a string
+        Dim mapName As New String(charArray)
+        MsgBox(mapName)
 
     End Sub
 

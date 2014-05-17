9bec4820bb7006e7845ad0c1c780481adc643718
Sean <shorton@worcester.edu>
2014-05-16 23:13:55 -0400

    Ready to take in txt file

--- TinySign/mapHandler.vb
+++ TinySign/mapHandler.vb
@@ -2,12 +2,13 @@
 
 Public Class mapHandler
 
+    'Converts byte array to a string
     Public Sub arrayToString(array As Byte())
         Dim charArray(20) As Char
 
         'put the byte array into a char array
         Dim index As Integer = 0
-        Do Until index >= 19
+        Do Until index = 19
             If array(index) = 0 Then Exit Do
             charArray(index) = Convert.ToChar(array(index))
             index += 1
@@ -15,9 +16,13 @@ Public Class mapHandler
 
         'make char array into a string
         Dim mapName As New String(charArray)
-        MsgBox(mapName)
+        compareToName(mapName)
 
     End Sub
 
+    'Compares map name from file to excel map 
+    Private Sub compareToName(name As String)
+
+    End Sub
 
 End Class
